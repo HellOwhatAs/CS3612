052d01825baa7bd9a6584ba3ac6d0cce097e69e8
HellOwhatAs <xjq701229@outlook.com>
2023-03-26 22:06:30 +0800

    finish coding

--- homework2/hw2/train.py
+++ homework2/hw2/train.py
@@ -7,55 +7,122 @@ X_train, X_test, Y_train, Y_test = get_data("./dataset/forestfires.csv")
 ######################## Implement you code here #######################
 ########################################################################
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
-X_train, X_test, Y_train, Y_test = X_train, X_test, Y_train.reshape(-1, 1), Y_test.reshape(-1, 1)
-lr = 1e-4
-N = 100000
+Y_train, Y_test = Y_train.reshape(-1, 1), Y_test.reshape(-1, 1)
 
-def Task1(*, output = True):
+def Task1(*, num_iter = 100000, lr = 1e-3, output = True):
     beta = np.random.random((X_train.shape[1], 1))
-    for _ in range(N):
+    for _ in range(num_iter):
         grad = -2 * X_train.T @ (Y_train - X_train @ beta)
-        grad /= np.abs(grad.sum())
+        grad /= np.sum(np.abs(grad))
         beta -= lr * grad
     loss = np.sum((Y_test - X_test @ beta) ** 2)
     if output: print(f"\nTask(1):\nbeta = {beta.flatten()}\nloss = {loss}\n")
     return beta, loss
-# Task1()
 
 def Task2(lmd, *, output = True):
     beta = np.linalg.inv(X_train.T @ X_train + lmd * np.eye(X_train.shape[1])) @ X_train.T @ Y_train
     loss = np.sum((Y_test - X_test @ beta) ** 2)
     if output: print(f"\nTask(2) with lmd = {lmd}:\nbeta = {beta.flatten()}\nloss = {loss}\n")
     return beta, loss
-# Task2(lmd = 0.5)
-# Task2(lmd = 1)
-# Task2(lmd = 100000)
 
-def Task3(lmd, gamma, *, output = True):
-    K = np.exp( - np.sum((X_train[:, np.newaxis] - X_train) ** 2, axis=2) / (2 * gamma **2))
+def Task3(lmd, sigma, *, output = True):
+    K = np.exp( - np.sum((X_train[:, np.newaxis] - X_train) ** 2, axis=2) / (2 * sigma **2))
     c = np.linalg.inv(K + lmd * np.eye(K.shape[0])) @ Y_train
-    K_test = np.exp( - np.sum((X_test[:, np.newaxis] - X_train) ** 2, axis=2) / (2 * gamma **2))
+    K_test = np.exp( - np.sum((X_test[:, np.newaxis] - X_train) ** 2, axis=2) / (2 * sigma **2))
     Y_pred = K_test @ c
     loss = np.sum((Y_test - Y_pred) ** 2)
-    if output: print(f"\nTask(3) with lmd = {lmd}, gamma = {gamma}:\nc = {c.flatten()}\nloss = {loss}\n")
+    if output: print(f"\nTask(3) with lmd = {lmd}, sigma = {sigma}:\nc = {c.flatten()}\nloss = {loss}\n")
     return c, loss
-# Task3(0.5, 10 ** 5)
-# Task3(10, 10 ** 5)
 
 def Task4():
-    """
-    Spline regression.
-    """
+    return 'task(4) 涉及高维的spline regression,  课堂上没有教。这一个task可以不用做。\n'
 
-def Task5(lmd, *, output = True):
+def Task5(lmd, *, num_iter = 100000, lr = 1e-4, output = True):
     beta = np.random.random((X_train.shape[1], 1))
-    for _ in range(N):
+    for _ in range(num_iter):
         grad = lmd * np.sign(beta) - X_train.T @ (Y_train - X_train @ beta)
-        grad /= np.abs(grad.sum())
+        grad /= np.sum(np.abs(grad))
         beta -= lr * grad
     loss = np.sum((Y_test - X_test @ beta) ** 2)
     if output: print(f"\nTask(5) with lmd = {lmd}:\nbeta = {beta.flatten()}\nloss = {loss}\n")
     return beta, loss
-# Task5(100)   
+
+if __name__ == '__main__':
+    task1, task2, task3, task4, task5 = 1,1,1,1,1
+    if task1:
+        plt.cla()
+        beta, loss = Task1()
+        beta = beta.flatten()
+        plt.bar(
+            range(len(beta)),
+            sorted(beta.flatten(), reverse=True)
+        )
+        plt.title("sorted $\\beta_i$ of Task(1)")
+        plt.savefig("./assets/task1.svg")
+
+    if task2:
+        plt.cla()
+        print("\nTask(2):")
+        best_beta, min_loss = None, float('inf')
+        for _lmd in range(1, 7):
+            lmd = 10 ** _lmd
+            beta, loss = Task2(lmd = lmd, output=False)
+            if min_loss > loss:
+                best_beta, min_loss = beta, loss
+            print('lambda = %-20s loss = %-20s' %(lmd, loss))
+            plt.plot(
+                sorted(beta.flatten(), reverse=True),
+                marker = 'o',
+                label = f"$\\lambda = 10^{_lmd}$"
+            )
+        print(f"\nbest_beta = {best_beta.flatten()}\nbest_loss = {min_loss}\n")
+        plt.legend()
+        plt.title("sorted $\\beta_i$ of Task(2)")
+        plt.savefig("./assets/task2.svg")
+
+    if task3:
+        plt.cla()
+        print("\nTask(3):")
+        best_beta, min_loss = None, float('inf')
+        for _lmd in range(1, 10, 2):
+            lmd = _lmd / 10
+            beta, loss = Task3(lmd = lmd, sigma = 1e5, output=False)
+            if min_loss > loss:
+                best_beta, min_loss = beta, loss
+            print('lambda = %-20s loss = %-20s' %(lmd, loss))
+            plt.plot(
+                sorted(beta.flatten(), reverse=True),
+                # marker = 'o',
+                label = f"$\\lambda = {lmd}$"
+            )
+        print(f"\nbest_c = {best_beta.flatten()}\nbest_loss = {min_loss}\n")
+        plt.legend()
+        plt.title("sorted $\\beta_i$ of Task(3) with $\\sigma = 10^5$")
+        plt.savefig("./assets/task3.svg")
+
+    if task4:
+        print("\nTask(4):")
+        print(Task4())
+
+    if task5:
+        plt.cla()
+        print("\nTask(5):")
+        best_beta, min_loss = None, float('inf')
+        for _lmd in range(2000, 3000, 200):
+            lmd = _lmd
+            beta, loss = Task5(lmd = lmd, output=False)
+            if min_loss > loss:
+                best_beta, min_loss = beta, loss
+            print('lambda = %-20s loss = %-20s' %(lmd, loss))
+            plt.plot(
+                sorted(beta.flatten(), reverse=True),
+                marker = 'o',
+                label = f"$\\lambda = {_lmd}$"
+            )
+        print(f"\nbest_beta = {best_beta.flatten()}\nbest_loss = {min_loss}\n")
+        plt.legend()
+        plt.title("sorted $\\beta_i$ of Task(5)")
+        plt.savefig("./assets/task5.svg")
--- homework2/report.md
+++ homework2/report.md
@@ -1,15 +1,51 @@
-## Task(1) Gradient descent on linear model
+### Task(1) Find a linear model to solve the regression problem.
+The MSE loss can be written as:
 $$
 L(\beta) = {\lVert Y - X\beta\rVert}_2^2
 $$
+The gradient can be written as:
 $$\begin{aligned}
     \frac{\partial L(\beta)}{\partial \beta} &= \frac{\partial {\lVert Y - X\beta\rVert}_2^2}{\partial \beta}\\
     &= -2 X^\top\left(Y-X\beta\right)
 \end{aligned}$$
+Using gradient descent to calculate $\beta$:
+$$
+\beta_{t+1} = \beta_t - \eta\cdot\left(-2 X^\top\left(Y-X\beta\right)\right)
+$$
+Where $\eta$ is learning rate.
+$$
+Error_\text{test} = \sum_{i=1}^m \left(y_1 - f\left(X_i\right)\right)^2
+$$
+After $100000$ iterations with $\eta = 0.001$, the $Error_\text{test}$ defined as above is $190.52631008965514$.
+The stdout is:
+```text
+Task(1):
+beta = [ 5.33052946e-01  4.12641248e-02  4.92870780e-02  1.44349983e-01
+  2.40886173e-02  2.75480488e-03  4.20735754e-03 -3.03859472e-04       
+ -2.86934505e-02 -3.71851433e-02 -1.35701831e-02  2.66545668e-02       
+  5.51986584e-01]
+loss = 190.52631008965514
+```
+And the visualization of the weight vector $\beta$ is: 
+![](./hw2/assets/task1.svg)
+### Task(2) Using Ridge Regression to solve the regression problem.
+```text
+Task(2):
+lambda = 10                   loss = 193.1719239298978
+lambda = 100                  loss = 190.64552450846662
+lambda = 1000                 loss = 186.57235083360067
+lambda = 10000                loss = 173.33544686440175
+lambda = 100000               loss = 168.967456043671
+lambda = 1000000              loss = 178.7360808699323
 
-## Task(2) Ridge regression
-
-## Task(3) RBF kernel regression
+best_beta = [ 8.21943627e-05  1.54299477e-03  1.00523867e-03  1.16273744e-03
+  3.83008956e-04  6.61266799e-03  1.45027551e-03  4.79238031e-04
+ -1.62703231e-03 -8.11543685e-04 -7.97680714e-04  1.13904484e-03
+ -1.46033335e-05]
+best_loss = 168.967456043671
+```
+![](./hw2/assets/task2.svg)
+### Task(3) Using RBF kernel regression to solve the regression problem.
 
 $$K(x_i, x_j) = \phi(x_i)^\top \phi(x_j)$$
 
@@ -80,10 +116,25 @@ $$\begin{aligned}
     c_n
 \end{bmatrix}
 \end{aligned}$$
+```text
+Task(3):
+lambda = 0.1                  loss = 155.29346939385917  
+lambda = 0.3                  loss = 155.1889666982272   
+lambda = 0.5                  loss = 155.1868258245468   
+lambda = 0.7                  loss = 155.19959272106766  
+lambda = 0.9                  loss = 155.21735678971228  
 
-## Task(4)
-
-## Task(5) Lasso regression
+best_c = [...]
+best_loss = 155.1868258245468
+```
+Case $c$ is too long, I won't put it in this report. $c$ can be obtained via executing the code.
+![](./hw2/assets/task3.svg)
+### Task(4) Using Spline Regression to solve the regression problem.
+```text
+Task(4):
+task(4) 涉及高维的spline regression,  课堂上没有教。这一个task可以不用做。
+```
+### Task(5) Using Lasso Regression to solve the regression problem.
 
 $$
 L(\beta) = \frac{1}{2}{\lVert Y-X \beta\rVert}_2^2 + \lambda {\lVert\beta\rVert}_1
@@ -93,3 +144,20 @@ $$\begin{aligned}
     \frac{\partial L(\beta)}{\partial\beta} &= \frac{\partial \frac{1}{2}{\lVert Y-X \beta\rVert}_2^2 + \lambda {\lVert\beta \rVert}_1}{\partial\beta}\\
     &= \lambda\cdot\text{sign}(\beta) - X^\top \left(Y-X\beta\right)
 \end{aligned}$$
+
+
+```text
+Task(5):
+lambda = 2000                 loss = 156.82043402173144  
+lambda = 2200                 loss = 179.0831038785226   
+lambda = 2400                 loss = 174.8449522295677   
+lambda = 2600                 loss = 175.93794339250366  
+lambda = 2800                 loss = 158.93595080304917  
+
+best_beta = [ 4.60576688e-01  3.43504798e-06 -3.74674312e-06  6.88011475e-06
+  1.14431192e-01 -1.60789531e-06 -3.12163388e-06  1.61820114e-04
+  8.20035892e-06  6.95914888e-06 -4.12246454e-06 -3.27022231e-06
+  1.87220832e-01]
+best_loss = 156.82043402173144
+```
+![](./hw2/assets/task5.svg)
