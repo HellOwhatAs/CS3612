ca309bb89aa0a1bdec98cc45cb8903cdc15f192c
HellOwhatAs <xjq701229@outlook.com>
2023-05-22 16:07:19 +0800

    LeNet & MyNet

--- Project/mandatory task/data_required_opt2/main.py
+++ Project/mandatory task/data_required_opt2/main.py
@@ -2,7 +2,94 @@ import os
 import numpy as np
 from dataset import get_data, normalize
 
+from tqdm import tqdm
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
 from matplotlib import pyplot as plt
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+class MyNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 9, padding=4)      # 1*32*32 -> 6*32*32
+        self.pool = nn.MaxPool2d(2, 2)                  # 6*32*32 -> 6*16*16
+        self.conv2 = nn.Conv2d(6, 16, 7, padding=3)     # 6*16*16 -> 16*16*16
+                                                        # 16*16*16 -> 16*8*8
+        self.conv3 = nn.Conv2d(16, 32, 5, padding=2)    # 16*8*8 -> 32*8*8
+                                                        # 32*8*8 -> 32*4*4
+        self.fc1 = nn.Linear(32 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+def train_and_test(netclass: type, *,epochs: int = 200):
+    net = netclass().to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    with tqdm(range(epochs), netclass.__name__) as tbar:
+        for epoch in tbar:
+            running_loss = 0.0
+            for inputs, labels in trainloader:
+
+                optimizer.zero_grad()
+
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                running_loss += loss.item()
+
+            tbar.set_postfix(loss = running_loss / len(trainloader))
+            tbar.update()
+            running_loss = 0.0
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+
+            outputs = net(images)
+
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return net, correct / total
 
 if __name__ == '__main__':
     ######################## Get train/test dataset ########################
@@ -21,3 +108,15 @@ if __name__ == '__main__':
     ########################################################################
     ######################## Implement you code here #######################
     ########################################################################
+
+    batch_size = 32
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    trainloader = torch.utils.data.DataLoader(list(zip(torch.from_numpy(X_train).to(device), torch.from_numpy(Y_train).to(device))), batch_size=batch_size,
+                                            shuffle=True)
+
+    testloader = torch.utils.data.DataLoader(list(zip(torch.from_numpy(X_test).to(device), torch.from_numpy(Y_test).to(device))), batch_size=batch_size,
+                                            shuffle=False)
+    
+    print(train_and_test(LeNet)[1], train_and_test(MyNet)[1])
