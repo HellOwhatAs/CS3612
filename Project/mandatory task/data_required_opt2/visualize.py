2b0f4ebbe5b6bdeab5a3fc786efed612635b6ad0
HellOwhatAs <xjq701229@outlook.com>
2023-05-24 10:30:52 +0800

    pca

--- Project/mandatory task/data_required_opt2/visualize.py
+++ Project/mandatory task/data_required_opt2/visualize.py
@@ -1,12 +1,12 @@
-import svgwrite, numpy as np, cv2, base64
+import svgwrite, numpy as np, cv2, base64, torch
 
-def images2svg(svg_filename: str, images: np.ndarray, pos: np.ndarray, *, width_pixels: int = 1000, imsize: float = 0.02):
+def images2svg(svg_filename: str, images: np.ndarray, pos: np.ndarray, *, width_pixels: int = 1600, imsize: float = 0.02):
     height_pixels = round(width_pixels * (pos[:, 1].max() - pos[:, 1].min()) / (pos[:, 0].max() - pos[:, 0].min()))
     dwg = svgwrite.Drawing(svg_filename, size=(width_pixels, height_pixels))
     dwg.viewbox(0, 0, width_pixels, height_pixels)
     dwg.add(dwg.rect(size=('100%', '100%'), fill="lightgrey", class_='background'))
     for i in range(pos.shape[1]):
-        pos[:, i] += pos[:, i].min()
+        pos[:, i] -= pos[:, i].min()
         pos[:, i] /= pos[:, i].max()
     for image, (x, y) in zip(images, pos):
         dwg.add(
@@ -18,11 +18,27 @@ def images2svg(svg_filename: str, images: np.ndarray, pos: np.ndarray, *, width_
         )
     return dwg
 
+def pca(X, no_dims: int = 2):
+    d = X.shape[1]
+    X -= torch.mean(X, 0)
+    l_c, M_c = torch.linalg.eig(torch.mm(X.t(), X))
+    i = 0
+    while i < d:
+        if l_c.imag[i] != 0:
+            M_c[:, i+1] = M_c[:, i]
+            i += 1
+        i += 1
+    return torch.mm(X, M_c[:, 0: no_dims].real)
+
 if __name__ == '__main__':
 
     from dataset import get_data
+    from cnns import LeNet, MyNet
     X_train, X_test, Y_train, Y_test = get_data('dataset')
+    lenet = torch.load('lenet.pth')
+    mynet = torch.load('mynet.pth')
 
-    X_train_pos = np.random.random((X_train.shape[0], 2))
-
-    images2svg("example_image.svg", np.squeeze(X_train * 255), X_train_pos).save(pretty=True)
+    with torch.no_grad():
+        out = lenet(torch.from_numpy(X_test).to(next(lenet.parameters()).device))
+    pos = pca(out).cpu().numpy()
+    images2svg("pca_lenet_final.svg", np.squeeze(X_test * 255), pos).save(pretty=True)
